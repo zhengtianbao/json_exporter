2f700af3160773d49acd712aa4547708fafc6af3
zhengtianbao <china.zhengtianbao@gmail.com>
2023-09-28 13:40:30 +0800

    update makefile build image

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -4,13 +4,14 @@ if [ ! -d "/var/lib/mysql/json_exporter" ]; then
     mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql
     mysqld --daemonize --user=mysql
     sleep 5
-    mysql -uroot -e "create database json_exporter default charset 'utf8' collate 'utf8_bin'; grant all on json_exporter.* to 'root'@'127.0.0.1' identified by '123456'; flush privileges;"
+    mysql -uroot < /opt/json_exporter.sql
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON json_exporter.* TO 'root'@'localhost' IDENTIFIED BY '123456'; flush privileges;"
 else
     mysqld --daemonize --user=mysql
 fi
 
 /usr/sbin/nginx &
-cd /usr/share/nginx/html/ && java -jar json_exporter-0.0.1-SNAPSHOT.jar &
+cd /usr/share/nginx/html/json_exporter && java -jar json_exporter-0.0.1-SNAPSHOT.jar &
 
 echo "ALL start!!!"
 tail -f /dev/null

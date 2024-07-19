b2912ad89e4064e1c91c5494688c667f0b30e127
zhengtianbao <china.zhengtianbao@gmail.com>
2024-07-19 10:26:04 +0800

    Simpleify the update logic
    at previous commit updateServer will update the existing records of headers belong to the server id.
    but server will have many OneToMany records, so this commit make update
    simple, just delete and insert new records. This may clean the code.

--- server/src/main/java/com/zhengtianbao/jsonexporter/service/ServerService.java
+++ server/src/main/java/com/zhengtianbao/jsonexporter/service/ServerService.java
@@ -1,13 +1,11 @@
 package com.zhengtianbao.jsonexporter.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.zhengtianbao.jsonexporter.model.Header;
 import com.zhengtianbao.jsonexporter.model.Server;
 import com.zhengtianbao.jsonexporter.repository.HeaderRepository;
 import com.zhengtianbao.jsonexporter.repository.ServerRepository;
@@ -45,32 +43,15 @@ public class ServerService {
 					server.setUrl(updatedServer.getUrl());
 					server.setMethod(updatedServer.getMethod());
 					server.setBody(updatedServer.getBody());
-
-					// Handle header updates
-					List<Header> newHeaders = updatedServer.getHeaderList();
-					List<Header> existingHeaders = server.getHeaderList();
-					List<Header> headersToRemove = existingHeaders.stream()
-							.filter(
-									header -> newHeaders.stream().noneMatch(newHeader -> newHeader.getValue().equals(header.getValue())))
-							.collect(Collectors.toList());
-					headersToRemove.forEach(header -> {
-						header.setServer(null);
-						existingHeaders.remove(header);
-						headerRepository.delete(header);
-					});
-					newHeaders.forEach(header -> {
-						if (existingHeaders.stream()
-								.noneMatch(existingHeader -> existingHeader.getValue().equals(header.getValue()))) {
-							header.setServer(server);
-							existingHeaders.add(header);
-						}
-					});
-					server.setHeaderList(existingHeaders);
+					updatedServer.getHeaderList().forEach(header -> header.setServer(server));
+					server.getHeaderList().clear();
+					server.getHeaderList().addAll(updatedServer.getHeaderList());
 					return serverRepository.save(server);
 				})
 				.orElseThrow(() -> new EntityNotFoundException("Server not found with id " + id));
 	}
 
+	@Transactional
 	public void deleteServer(Long id) {
 		if (serverRepository.existsById(id)) {
 			serverRepository.deleteById(id);
